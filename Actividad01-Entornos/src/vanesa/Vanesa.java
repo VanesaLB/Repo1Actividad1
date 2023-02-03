9a535cfa525b42d97c170772281f70429df65d87
vanesa.lido <vanesa.lido@gmail.com>
2023-02-03 16:34:47 +0100

    añado los 3 hobbies en mi documento java.

--- Actividad01-Entornos/src/vanesa/Vanesa.java
+++ Actividad01-Entornos/src/vanesa/Vanesa.java
@@ -4,6 +4,7 @@ public class Vanesa {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Vanesa");
+		System.out.println("Me encanta hacer senderismo, ir en bicicleta y ver películas o series aunque últimamente no me da la vida.");
 
 	}
 
