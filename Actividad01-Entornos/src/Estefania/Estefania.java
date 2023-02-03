35de516d4e7c6b2af3c9adb3b9a5ef4de6fe9f33
estefania.ballesteros <fanybt2010.ebt@gmail.com>
2023-02-03 21:29:41 +0100

    añadido hobbies

--- Actividad01-Entornos/src/Estefania/Estefania.java
+++ Actividad01-Entornos/src/Estefania/Estefania.java
@@ -7,4 +7,8 @@ public class Estefania {
 		System.out.println("Hola , soy Estefania ");
 		
 	}
+	public void mostrarHobbies () {
+		System.out.println("Mis hobbies son leer , bailar , deporte ");
+	}
 }
+
