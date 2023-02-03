9cbbbd33598579f6bfdac33287c47a3e7a1ab669
A.Sanchez <bereberto25@gmail.com>
2023-02-03 16:05:53 +0100

    Añadido 3 hobbies que me gustan al codigo

--- Actividad01-Entornos/src/alberto/Alberto.java
+++ Actividad01-Entornos/src/alberto/Alberto.java
@@ -4,12 +4,19 @@ public class Alberto {
 	  public static void main(String[] args) {
 	    Alberto alberto = new Alberto();
 	    alberto.presentacion();
+	    alberto.mostrarHobbies();
 	  }
-
 
 	public void presentacion() {
 	    System.out.println("Hola, soy Alberto");
 	  }
+	
+	 public void mostrarHobbies() {
+		    System.out.println("Mis hobbies son: ");
+		    System.out.println("Leer");
+		    System.out.println("Escuchar música");
+		    System.out.println("Hacer deporte");
+		  }
 	
 		}
 	
