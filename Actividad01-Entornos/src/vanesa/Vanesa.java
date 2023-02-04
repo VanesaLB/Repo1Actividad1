44206dd836ccb4e4fb464058f586ddc21b9fee4b
vanesa.lido <vanesa.lido@gmail.com>
2023-02-04 17:44:01 +0100

    mis tres comidas favoritas

--- Actividad01-Entornos/src/vanesa/Vanesa.java
+++ Actividad01-Entornos/src/vanesa/Vanesa.java
@@ -5,7 +5,7 @@ public class Vanesa {
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Vanesa");
 		System.out.println("Me encanta hacer senderismo, ir en bicicleta y ver películas o series aunque últimamente no me da la vida.");
-
+		System.out.println("Mis 3 comidas favoritas son la fideuá, pizza y el arroz al horno.");
 	}
 
 }
