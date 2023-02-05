699e70cb2959c310f90e4bb65ce7e8e38529bf8d
Cesariko <cesariko281@gmail.com>
2023-02-05 18:34:34 +0100

    Pongo las 3 comidas

--- Actividad01-Entornos/src/Cesar/Cesar.java
+++ Actividad01-Entornos/src/Cesar/Cesar.java
@@ -4,8 +4,8 @@ public class Cesar {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, soy César");
-		System.out.println("");
-
+		System.out.println("Hobbies: Caminar, escuchar música, leer");
+		System.out.println("Comidas favoritas: Pollo, Arroz, Tortilla");
 
 	}
 
